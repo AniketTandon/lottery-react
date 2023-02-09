4c035ddbe23a138a587bacd6e9b1975461cf8ad3
Aniket <aniket.tandon@byjus.com>
2023-02-10 03:02:07 +0530

    first commit

---
+++ src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+const address='0x65b787Cbfa2526E551650BF0536039B5f77B59b5';
+
+const abi=[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+//The purpose of this file is to create a local instance of the contract
+export default new web3.eth.Contract(abi, address);
